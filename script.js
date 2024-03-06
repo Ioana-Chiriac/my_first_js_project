c9deb0dcc1441d7b5d6f1795298d53c7f0eb65e1
Ioana-Chiriac <ioana.chiriac1014@gmail.com>
2024-03-06 18:15:00 +0200

    update cod

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ console.log(age);
 
 let name = 'David';
 console.log('name: ');
-console.log(username);
+
+
+// operatori date numerice
+
+let firstNumber = 10;
+firstNumber = 12;
+
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
+
+// scadere
+let pastAge = age - years;
+console.log(`Acum ${years} ani aveam varsta de ${pastAge}`);
+
